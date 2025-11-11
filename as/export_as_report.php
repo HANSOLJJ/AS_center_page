bfed1d286d86e9d89defa2fbf13cf2046a3fdc65
mic4u <dev@mic4u.com>
2025-11-11 15:27:36 +0900

    refactor: 불필요한 s13_total_cost 필드 삭제 및 ex_total_cost로 통일
    DB 변경:
    - step13_as에 ex_total_cost 필드 추가 (ex_total_cost를 복원)
    - s13_total_cost 필드 삭제 (중복된 불필요한 필드)
    
    PHP 코드 변경:
    - export_as_report.php SELECT 쿼리에서 s13_total_cost → ex_total_cost
    - export_as_report.php M열에서 s13_total_cost → ex_total_cost (2곳)
    
    ex_total_cost가 step13_as의 유일한 총액 필드로 통일됨

--- as/export_as_report.php
+++ as/export_as_report.php
@@ -77,7 +77,7 @@ $query = "SELECT
     ex_company,
     ex_sec1,
     DATE_FORMAT(s13_as_out_date, '%Y-%m-%d') as as_out_date,
-    s13_total_cost,
+    ex_total_cost,
     s13_bankcheck_w,
     COALESCE(ex_tel, '') as ex_tel,
     s13_tax_code
@@ -198,7 +198,7 @@ while ($as = mysql_fetch_assoc($result)) {
         $sheet->setCellValue('J' . $row, '');
         $sheet->setCellValue('K' . $row, '');
         $sheet->setCellValue('L' . $row, '');
-        $sheet->setCellValue('M' . $row, isset($as['s13_total_cost']) ? (int)$as['s13_total_cost'] : '');
+        $sheet->setCellValue('M' . $row, isset($as['ex_total_cost']) ? (int)$as['ex_total_cost'] : '');
         $sheet->setCellValue('N' . $row, $payment_method);
         $sheet->setCellValue('O' . $row, $as['ex_tel']);
         $sheet->setCellValue('P' . $row, $tax_display);
@@ -233,7 +233,7 @@ while ($as = mysql_fetch_assoc($result)) {
             $sheet->setCellValue('J' . $row, $part['cost_name'] ?? '');
             $sheet->setCellValue('K' . $row, (isset($part['s18_quantity']) ? $part['s18_quantity'] . '개' : ''));
             $sheet->setCellValue('L' . $row, (int)$part_price);
-            $sheet->setCellValue('M' . $row, $is_first ? (isset($as['s13_total_cost']) ? (int)$as['s13_total_cost'] : '') : '');
+            $sheet->setCellValue('M' . $row, $is_first ? (isset($as['ex_total_cost']) ? (int)$as['ex_total_cost'] : '') : '');
             $sheet->setCellValue('N' . $row, $is_first ? $payment_method : '');
             $sheet->setCellValue('O' . $row, $is_first ? $as['ex_tel'] : '');
             $sheet->setCellValue('P' . $row, $is_first ? $tax_display : '');
