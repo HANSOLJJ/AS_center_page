5ffd8509df84c9d8221269a5716a52255e44331e
mic4u <dev@mic4u.com>
2025-11-11 16:47:34 +0900

    fix: add tab=completed parameter to restore link in completed tab
    - completed 탭의 이전 버튼에 &tab=completed 파라미터 추가
    - as_repair_handler.php에서 올바른 탭 감지 가능
    - 완료 필드 초기화 로직이 제대로 실행됨

--- as/as_requests.php
+++ as/as_requests.php
@@ -1116,7 +1116,7 @@ function getStatusColor($level)
                                     </td>
                                 <?php elseif ($current_tab === 'completed'): ?>
                                     <td rowspan="<?php echo $rowspan; ?>">
-                                        <a href="as_repair_handler.php?action=restore&itemid=<?php echo $items[0]['s14_aiid']; ?>"
+                                        <a href="as_repair_handler.php?action=restore&itemid=<?php echo $items[0]['s14_aiid']; ?>&tab=completed"
                                             class="action-btn edit"
                                             onclick="return confirm('수리 작업을 초기화하고 요청 탭으로 되돌리시겠습니까?');">이전</a>
                                     </td>
